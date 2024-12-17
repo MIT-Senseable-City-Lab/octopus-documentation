2179eab753b6e6a376dfc5898776b7f81202dd2c
aasehaa <ase.hatveit@gmail.com>
2024-12-17 15:12:52 +0100

    changed social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -58,7 +58,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg', // TODO: change the social card!
+      image: 'img/octopus-social-card.jpeg', // TODO: change the social card!
       navbar: {
         logo: {
           alt: 'Senseable City Lab',
